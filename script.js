18b12260bcdbe74064c8165269122afe01880feb
yassir <elatmaniyassir3@gmail.com>
2020-09-28 17:38:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// const inputs=document.querySelectorAll(".controls input");
+//     function handleChange(){
+//         //const suffix=inputs.dataset.sizing;
+//         console.log(this.dataset);
+//     }
+   
+//     inputs.forEach(input=>input.addEventListener("change",handleChange));
+//     inputs.forEach(input=>input.addEventListener("mousemove",handleChange));
+//     const radius=document.querySelector("radius");
+//     const res=document.querySelector("range_weight_disp");
+//     radius.addEventListener("input",function(){
+//        res.innerHTML=radius.value;
+//     })
